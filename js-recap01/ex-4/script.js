b1996ea7d446c9f9ec1507a128ab4366bb63721b
LeonardaH <93203715+LeonardaH@users.noreply.github.com>
2022-11-22 20:03:34 +0100

    js-recap01

--- js-recap01/ex-4/script.js
+++
@@ -1,9 +0,0 @@
-//write a function to get a value of the href, hreflang, rel, target, and type attributes of the specidied link when clicking on the button (show results in the console)
-
-function getAttributes() {
-  let href = document.getElementById("w3r").href;
-  let hreflang = document.getElementById("w3r").hreflang;
-  let rel = document.getElementById("w3r").rel;
-  let target = document.getElementById("w3r").target;
-  let type = document.getElementById("w3r").type;
-}
